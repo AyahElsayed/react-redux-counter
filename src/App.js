c3dc67ba9d2d2e57f8f1cb46b42ad1f89980c251
Ayah <eng.aya.elsayed.2020@gmail.com>
2022-05-30 17:56:15 +0200

    subscribe to store

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { useSelector } from 'react-redux';
 import './App.css';
 
 function App() {
+  const state = useSelector((state) => {
+    if (state.value < 1) {
+      return 'no number'
+    }
+    return state.value
+  })
+
   return (
     <div className='App'>
       <h1>Hello Redux Basic</h1>
-      <div className='counter'>Counter: 0</div>;
+      <div className='counter'>Counter: {state}</div>;
       <div>
         <button className='btn'>increase +</button>
         <button className='btn'>decrease -</button>
