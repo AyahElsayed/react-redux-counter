67783bbca159be1d9c7a630b81d9097f2dfed81c
Ayah <eng.aya.elsayed.2020@gmail.com>
2022-06-07 00:47:46 +0200

    Redux Toolkit - Create Slice part 1

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,37 @@
 import { createStore } from 'redux'
+import { createSlice } from '@reduxjs/toolkit'
 
 const initState = { value: 0, showCounter: false }
 
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: initState,
+  reducers: {
+    increase: (state, action) => {
+      state.value += 1;
+    },
+    decrease: (state, action) => {
+      state.value -= 1;
+    },
+  }
+})
+
 const countReducer = (state = initState, action) => {
-  // console.log(action)
+    // console.log(action)
 
-  if (action.type === 'increase') {
-    return { ...state, value: state.value + action.payLoad }
-  }
+    if (action.type === 'increase') {
+      return { ...state, value: state.value + action.payLoad }
+    }
 
-  if (action.type === 'decrease') {
-    return { ...state, value: state.value - action.payLoad }
-  }
+    if (action.type === 'decrease') {
+      return { ...state, value: state.value - action.payLoad }
+    }
 
-  if (action.type === 'toggleCounter') {
-    return { ...state, showCounter: !state.showCounter}
+    if (action.type === 'toggleCounter') {
+      return { ...state, showCounter: !state.showCounter }
+    }
+    return state
   }
-  return state
-}
 
 
 const store = createStore(countReducer)
