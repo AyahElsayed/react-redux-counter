10239c62eb224a30c40594363a4236b661ab6d51
Ayah <eng.aya.elsayed.2020@gmail.com>
2022-06-07 00:54:58 +0200

    Create Slice part 2

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,6 @@
-import { createStore } from 'redux'
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createStore } from '@reduxjs/toolkit'
 
-const initState = { value: 0, showCounter: false }
+const initState = { value: 0 }
 
 const counterSlice = createSlice({
   name: 'counter',
@@ -16,24 +15,24 @@ const counterSlice = createSlice({
   }
 })
 
-const countReducer = (state = initState, action) => {
-    // console.log(action)
+// const countReducer = (state = initState, action) => {
+//     // console.log(action)
 
-    if (action.type === 'increase') {
-      return { ...state, value: state.value + action.payLoad }
-    }
+//     if (action.type === 'increase') {
+//       return { ...state, value: state.value + action.payLoad }
+//     }
 
-    if (action.type === 'decrease') {
-      return { ...state, value: state.value - action.payLoad }
-    }
+//     if (action.type === 'decrease') {
+//       return { ...state, value: state.value - action.payLoad }
+//     }
 
-    if (action.type === 'toggleCounter') {
-      return { ...state, showCounter: !state.showCounter }
-    }
-    return state
-  }
+//     if (action.type === 'toggleCounter') {
+//       return { ...state, showCounter: !state.showCounter }
+//     }
+//     return state
+//   }
 
 
-const store = createStore(countReducer)
+const store = createStore(counterSlice.reducer)
 
 export default store
